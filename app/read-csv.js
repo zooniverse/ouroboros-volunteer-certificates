e5e5b60935b112c45675ee6e7334abe74829162a
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-04-21 22:36:43 +0100

    readCSV now reads each volunteer's project contributions

--- app/read-csv.js
+++ app/read-csv.js
@@ -6,14 +6,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default async function readCSV (filename) {
   return new Promise((resolve, reject) => {
+    // List of volunteers and the projects they've worked on.
     const volunteers = []
 
     fs.createReadStream(path.resolve(__dirname, '..', filename))
     .pipe(csv.parse({ headers: true }))
     .on('error', error => console.error(error))
     .on('data', (row) => {
+      // Find the volunteer.
+      // If volunteer doesn't exist, add them to the list
       let volunteer = volunteers.find(vol => vol.user_id === row.user_id)
-
       if (!volunteer) {
         volunteer = {
           user_id: row?.user_id,
@@ -25,6 +27,15 @@ export default async function readCSV (filename) {
         volunteers.push(volunteer)
       }
 
+      // Add the project to the volunteer's list of projects.
+      let project = volunteer.projects.find(proj => proj.project_id === row.project_id)
+      if (!project) {
+        volunteer.projects.push({
+          project_id: row.project_id,
+          project_display_name: row.project_display_name,
+          total_count: row.total_count,
+        })
+      }
     })
     .on('end', (rowCount) => {
       resolve(volunteers)
