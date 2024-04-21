3551613708a1d2ab310eec64b01cee4a2b0910e1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-04-21 21:24:39 +0100

    Separate ZooniverseCertificate function into own file

---
+++ app/zooniverse-certificate.js
@@ -0,0 +1,64 @@
+/*
+Zooniverse Certificate "React Component"/PDF template
+Ref: https://react-pdf.org/
+
+- Note that the PDF stylesheet has slight differences from CSS, e.g. pixels
+  aren't valid units.
+- I couldn't properly set up JSX syntax, so I'm using React.createElement().
+ */
+
+import { createElement as elem } from 'react'
+import { Page, Text, View, Image, Document, StyleSheet } from '@react-pdf/renderer'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    fontSize: '10mm',
+    backgroundColor: '#202020',
+  },
+  main: {
+    margin: '20mm',
+    padding: '20mm',
+    flexGrow: 1,
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    border: '2mm solid #00979d',
+  },
+  zooLogo: {
+    display: 'block',
+    width: '50mm',
+    margin: '0 auto',
+  },
+  bigText: {
+    fontSize: '16mm',
+    textAlign: 'center',
+  },
+  smallText: {
+    textAlign: 'center',
+  },
+})
+
+const ZooniverseCertificate = ({ name }) => (
+  elem(
+    Document, null,
+    elem(Page, { size: 'A4', style: styles.page },
+      elem(View, { style: styles.main },
+        elem(Image, { style: styles.zooLogo, src: `${__dirname}/../assets/zooniverse-logo-teal.png` }),
+        elem(Text, { style: styles.bigText },
+          name
+        ),
+        elem(Text, { style: styles.smallText },
+          'is a Zooniverse volunteer'
+        )
+      )
+    )
+  )
+)
+
+export default ZooniverseCertificate
